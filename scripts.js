b1478158ae5a51a6ab5c927e687b935ccd4ef668
Alex I <alextheclaw@hotmail.com>
2022-08-12 17:52:45 -0700

    Got the listener to work and am on step 4.

--- scripts.js
+++ scripts.js
@@ -5,5 +5,9 @@ for (let i = 1; i < 257; i++) {
   const div = document.createElement("div");
   div.className = "grid-div";
 
+  div.addEventListener("mouseover", () => {
+    div.setAttribute("style", "background-color: blue;");
+  });
+
   container.appendChild(div);
 }
