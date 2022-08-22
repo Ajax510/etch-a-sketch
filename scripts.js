faca380747a47aaba7bfbfabb81d3f0805fee333
Alex I <alextheclaw@hotmail.com>
2022-08-21 19:13:31 -0700

    Rebuilt entire function from scratch

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
-const container = document.querySelector(".container");
+const container = document.querySelector("#container");
+const button = document.querySelector(".setSize");
 
-// loop to create the needed divs
-for (let i = 1; i < 257; i++) {
+function getGridSize() {
+  let userChoice = prompt(`Please choose a number between 1 - 100.`);
+  userChoice *= userChoice;
+  return userChoice;
+}
+
+button.addEventListener("click", getGridSize);
+
+let gridSize = getGridSize();
+console.log(gridSize);
+
+for (let i = 1; i <= gridSize; i++) {
   const div = document.createElement("div");
-  div.className = "grid-div";
+
+  div.classList.add("divs");
 
   div.addEventListener("mouseover", () => {
-    div.setAttribute("style", "background-color: blue;");
+    div.classList.add("background");
   });
 
   container.appendChild(div);
