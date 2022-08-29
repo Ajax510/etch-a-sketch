6b2b474e623e763888d2272aae574d4909c02206
Alex I <alextheclaw@hotmail.com>
2022-08-28 19:42:55 -0700

    Game working, need to fix restart after each round

--- scripts.js
+++ scripts.js
@@ -1,25 +1,43 @@
+// selecting the container ID
 const container = document.querySelector("#container");
+// selecting the setSize class
 const button = document.querySelector(".setSize");
 
+// function will get the number of square divs to create and return the number
 function getGridSize() {
   let userChoice = prompt(`Please choose a number between 1 - 100.`);
-  userChoice *= userChoice;
-  return userChoice;
+  grid(userChoice);
 }
 
+// upon clicking the button, this will initiate the getGridSize function
 button.addEventListener("click", getGridSize);
 
-let gridSize = getGridSize();
-console.log(gridSize);
+// grid function will take in the number of divs to create
+function grid(gridSize) {
+  const gridArea = gridSize * gridSize;
+  // this will use the number of divs andcreate an equal number of rows and columns
+  container.setAttribute(
+    "style",
+    `display: grid;
+    grid-template-columns: repeat(${gridSize}, 50px); 
+    grid-template-rows: repeat(${gridSize}, 50px);`
+  );
 
-for (let i = 1; i <= gridSize; i++) {
-  const div = document.createElement("div");
+  // this loop will create the number of divs using the number given.
+  for (let i = 1; i <= gridArea; i++) {
+    // every iteration will create the div needed
+    const div = document.createElement("div");
 
-  div.classList.add("divs");
+    // give it the class of div so the border can be seen.
+    div.classList.add("divs");
 
-  div.addEventListener("mouseover", () => {
-    div.classList.add("background");
-  });
+    // each div will have an event listener
+    div.addEventListener("mouseover", () => {
+      // event listener will give each div the class of background\
+      div.classList.add("background");
+    });
 
-  container.appendChild(div);
+    // will add the div element to the container
+    container.appendChild(div);
+  }
 }
